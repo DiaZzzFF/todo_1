a9d6a3029e1e102b6e2f405818a580b50b8bbc85
DiaZzzFF <dvsh.dreamer@gmail.com>
2020-10-07 17:43:44 +0300

    Добавляет эмуляцию задержки сервера + Loader

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import React, {useEffect} from "react";
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
 import AddTodo from "./Todo/AddTodo";
+import Loader from "./Loader";
 
 
 const App = () => {
   const [todos, setTodos] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  const noTodos = (loading ? null : <p>No todos!</p>);
 
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/todos?_limit=5`)
       .then((response) => response.json())
       .then((mocks) => {
-        setTodos(mocks);
+        setTimeout(() => {
+          setTodos(mocks);
+          setLoading(false);
+        }, 2000);
       });
   }, []);
 
@@ -47,10 +53,13 @@ const App = () => {
 
         <AddTodo onCreate={addTodo} />
 
-        {todos.length
-          ? <TodoList todos={todos} btnToggle={toggleTodo} />
-          : <p>No todos!</p>
-        }
+        {loading && <Loader />}
+
+        {todos.length ? (
+          <TodoList todos={todos} btnToggle={toggleTodo} />
+        ) : (
+          noTodos
+        )}
       </div>
     </Context.Provider>
   );
