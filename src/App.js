f76ffab7b62aef464f79af0b5e88463c8159f7bc
DiaZzzFF <dvsh.dreamer@gmail.com>
2020-10-07 17:14:38 +0300

    Добавляет хук useEffect для эмуляции данных с сервера

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
@@ -6,33 +6,15 @@ import AddTodo from "./Todo/AddTodo";
 
 
 const App = () => {
-  const [todos, setTodos] = React.useState([
-    {
-      id: 1,
-      completed: false,
-      title: `Оперативная память`,
-    },
-    {
-      id: 2,
-      completed: false,
-      title: `SSD`,
-    },
-    {
-      id: 3,
-      completed: false,
-      title: `HDD`,
-    },
-    {
-      id: 4,
-      completed: false,
-      title: `Процессор`,
-    },
-    {
-      id: 5,
-      completed: false,
-      title: `Кулер`,
-    },
-  ]);
+  const [todos, setTodos] = React.useState([]);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/todos?_limit=5`)
+      .then((response) => response.json())
+      .then((mocks) => {
+        setTodos(mocks);
+      });
+  }, []);
 
   const toggleTodo = (id) => {
     setTodos(todos.map((todo) => {
